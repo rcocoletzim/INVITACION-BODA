f4bc3f8745725017a8f288ebc6618fa87eab237b
Ruben Cocoletzi <65933865+rcocoletzim@users.noreply.github.com>
2026-01-23 20:19:31 -0600

    Actualizar listaInvitados.ts
    Silva mata familia

--- src/data/listaInvitados.ts
+++ src/data/listaInvitados.ts
@@ -8,7 +8,7 @@ interface InfoInvitado {
 // Aquí es donde defines los códigos.
 // La clave (izquierda) es el código que el invitado escribe.
 export const dbInvitados: Record<string, InfoInvitado> = {
-  "FAMILIA-PEREZ": { nombre: "Familia Pérez", pases: 4 },
+  "SILVA-MATA": { nombre: "Familia Silva Mata", pases: 5 },
   "JUAN-LOPEZ":    { nombre: "Juan López y Acompañante", pases: 2 },
   "SOLO-YO":       { nombre: "Mi Mejor Amigo", pases: 1 },
   "TEST1234":      { nombre: "Invitado de Prueba", pases: 5 },
